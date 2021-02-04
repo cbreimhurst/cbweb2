d8d1a65a9f723496e77c7f6eb69495661669e1de
Chris Breimhurst <cbreimhurst@gmail.com>
2021-02-04 08:47:15 -0500

    trying to hook up blog

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,60 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Chris Breimhurst"/>
+       <main>
+         <h1>{{msg}}</h1>
+        <!-- <div class="post" v-for="post in sortedPosts" :key="post.id">
+          <h3>
+            <a :href="`blog/${post.slug}`">{{ post.title.rendered }}</a>
+          </h3>
+          <small>{{ post.date }}</small>
+          <div v-html="post.excerpt.rendered"></div>
+          <a :href="`blog/${post.slug}`" class="readmore slide">Read more ⟶</a>
+        </div> -->
+      </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data() {
+    return {
+      msg: 'Chris Breimhurst',
+      // selectedTag: null,
+      // activeClass: "active"
+    };
+  },
+  computed: {
+    // posts() {
+    //   return this.$store.state.posts;
+    // },
+    // tags() {
+    //   return this.$store.state.tags;
+    // },
+    // sortedPosts() {
+    //   if (!this.selectedTag) return this.posts;
+    //   return this.posts.filter(el => el.tags.includes(this.selectedTag));
+    // }
+  },
+  created() {
+   // this.$store.dispatch("getPosts");
+  },
+  methods: {
+    // updateTag(tag) {
+    //   if (!this.selectedTag) {
+    //     this.selectedTag = tag.id;
+    //   } else {
+    //     this.selectedTag = null;
+    //   }
+    // },
+      // increment() {
+      //   this.$store.commit('increment')
+      //   console.log(this.$store.state.count)
+      // }
   }
 }
 </script>
@@ -40,7 +84,7 @@ h1 {
 	word-break: break-all;
 	line-height: 1;
 	text-align: left;
-    background: url(assets/bg.jpeg) 0 0 / cover no-repeat;
+    background: url(/img/chris.b238fc8c.png) 0 0 / 40% repeat;
    -webkit-background-clip: text;
    -webkit-text-fill-color: transparent;
     background-clip: text;
--- src/store/index.js
+++ src/store/index.js
@@ -1,66 +1,31 @@
-const siteURL = "https://data.christopherbreimhurst.com"
+const siteURL = "https://data.christopherbreimhurst.com/wp-json/wp/v2/posts?page=1&per_page=20&_embed=1"
 
-export const state = () => ({
-  posts: [],
-  tags: []
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(Vuex)
+Vue.use(VueAxios, axios)
+
+export default new Vuex.Store({
+  state: {
+     coins: []
+  },
+  actions: {
+    loadCoins () {
+      axios
+        .get(siteURL)
+        .then(r => r.data)
+        .then(coins => {
+        console.log(coins)
+        })
+    }
+  },
+  mutations: {
+    SET_COINS (state, coins) {
+      state.coins = coins
+    
+    }
+  }
 })
-
-export const mutations = {
-  updatePosts: (state, posts) => {
-    state.posts = posts
-  },
-  updateTags: (state, tags) => {
-    state.tags = tags
-  }
-}
-
-export const actions = {
-  async getPosts({ state, commit, dispatch }) {
-    if (state.posts.length) return
-
-    try {
-      let posts = await fetch(
-        `${siteURL}/wp-json/wp/v2/posts?page=1&per_page=20&_embed=1`
-      ).then(res => res.json())
-
-      posts = posts
-        .filter(el => el.status === "publish")
-        .map(({ id, slug, title, excerpt, date, tags, content }) => ({
-          id,
-          slug,
-          title,
-          excerpt,
-          date,
-          tags,
-          content
-        }))
-
-      commit("updatePosts", posts)
-    } catch (err) {
-      console.log(err)
-    }
-  },
-  async getTags({ state, commit }) {
-    if (state.tags.length) return
-
-    let allTags = state.posts.reduce((acc, item) => {
-      return acc.concat(item.tags)
-    }, [])
-    allTags = allTags.join()
-
-    try {
-      let tags = await fetch(
-        `${siteURL}/wp-json/wp/v2/tags?page=1&per_page=40&include=${allTags}`
-      ).then(res => res.json())
-
-      tags = tags.map(({ id, name }) => ({
-        id,
-        name
-      }))
-
-      commit("updateTags", tags)
-    } catch (err) {
-      console.log(err)
-    }
-  }
-}
