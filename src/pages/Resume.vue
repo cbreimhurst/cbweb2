6e79ba8063ea3184b7f015cf2d1557a4247bec08
cbreimhurst <cbreimhurst@gmail.com>
2024-10-18 13:17:02 -0400

    styling stuff

--- src/pages/Resume.vue
+++ src/pages/Resume.vue
@@ -10,7 +10,7 @@
     </p>
     <p><small>Updated: October 18, 2024</small></p>
 
-      <section>
+      <section id="intro">
     
     <ul>
         <li>Web Developer­</li>
@@ -20,91 +20,113 @@
 </section>
 
 
- <section>
+ <section id="competencies">
      
      <h2>Core Competencies:</h2>
      
-     <p><strong>User-Centered Design:</strong> Commitment to understanding user behaviors and needs through thorough research and user testing, ensuring that designs are tailored to exceed user expectations.</p>
-     <p><strong>Wireframing and Prototyping:</strong> Proficient in using tools like Sketch, Figma, and Adobe XD to create wireframes and interactive prototypes, enabling iterative design improvements.</p>
-     <p><strong>Collaborative Team Player:</strong> Excellence in cross-functional collaboration, working closely with developers, product managers, and stakeholders to ensure alignment and successful project outcomes.</p>
-     <p><strong>Design Systems:</strong> Experience in creating and maintaining design systems to ensure consistency and scalability across all product interfaces.</p>
-     <p><strong>Responsive Design:</strong> Skilled in designing for various platforms, including web and mobile, to deliver seamless user experiences across devices.</p>
+     <article>
+         <p><strong>User-Centered Design:</strong> Commitment to understanding user behaviors and needs through thorough research and user testing, ensuring that designs are tailored to exceed user expectations.</p>
+         <p><strong>Wireframing and Prototyping:</strong> Proficient in using tools like Sketch, Figma, and Adobe XD to create wireframes and interactive prototypes, enabling iterative design improvements.</p>
+         <p><strong>Collaborative Team Player:</strong> Excellence in cross-functional collaboration, working closely with developers, product managers, and stakeholders to ensure alignment and successful project outcomes.</p>
+         <p><strong>Design Systems:</strong> Experience in creating and maintaining design systems to ensure consistency and scalability across all product interfaces.</p>
+         <p><strong>Responsive Design:</strong> Skilled in designing for various platforms, including web and mobile, to deliver seamless user experiences across devices.</p>
+     </article>
      
  </section>
- <section>
+ <section id="experieince">
      
      <h2>Professional Experience:</h2>
      
-     <h3>Pennant Creatives</h3>
-     <p>
-        Founder, Web Developer & Designer<br>
-        July 2013 – July 2024
-     </p>
-     
-     
-     <p><strong>Strategic Planning:</strong> Weekly review of agency's long-term goals and objectives. Strategized how daily activities contribute to these goals.</p>
-     <p><strong>Client Communication:</strong> Communicated with clients to discuss project updates, gather feedback, and address any concerns or questions they may have.</p>
-     <p><strong>Team Management:</strong> Checked in with the creative team to discuss project progress, assign tasks, and provide guidance and feedback.</p>
-     <p><strong>Project Management:</strong> Monitored ongoing projects to ensure they were on track, within budget, and meeting client expectations.</p>
-     <p><strong>Client Pitches & RFPs:</strong> Collaborated on presentations for client pitches and Requests for Proposals (RFPs), contributing technical insights and helping to showcase the company’s capabilities and value proposition.</p>
-     <p><strong>Creative Direction:</strong> Provided creative direction and input on projects, helping to generate innovative ideas and solutions.</p>
-     <p><strong>Financial Management:</strong> Reviewed financial statements, budgeting, and invoicing to ensure the agency's financial solvency.</p>
-     <p><strong>Quality Control:</strong> Ensured that the quality of work produced was consistent and met or exceeded industry standards.</p>
-     <p><strong>Web Development:</strong> Built all client sites on WordPress using the GeneratePress theme and GenerateBlocks theme extension.</p>
-     
-     <h3>PlannedGiving.com</h3>
-     <p>
-        Web Services<br>
-        July 2014 – October 2024
-     </p>
-     
-     <p><strong>Design and Template Creation:</strong> Replicated the design and user interface of the organization's website, including layout, color schemes and fonts, to create a customizable template for the dynamic integration of Planned Giving marketing content.</p>
-     <p><strong>Content Management:</strong> Regularly updated and modified content based on customer requests received via email or phone, ensuring timely and accurate adjustments.</p>
-     <p><strong>Brand Consistency:</strong> Ensured seamless user experience throughout all updates and improvements.</p>
-     <p><strong>Feature Development:</strong> Added new features to the proprietary platform to improve both the developer experience and the product offering, enhancing usability, efficiency, and overall functionality. Rewrote legacy styles and scripts when newer Web APIs became available. Platform was built in PHP and MySQL.</p>
-     
-     <h2>Additional Experience:</h2>
-     
-     <p><strong>Kimberton Whole Foods</strong>, February 2009 - June 2013 (intermittently)<br>
-        Assistant Store Coordinator - Downingtown location,<br>
-        Perishables Supervisor - Warehouse location</p>
-     <p><strong>Lancaster Farm Fresh Cooperative</strong>, June 2011 - June 2012<br>
-        Operational Coordinator for Community Supported Agriculture (CSA) Program</p>
-     <p><strong>Home Field Advantage, Ltd.</strong>, January 1998 - August 2006 (intermittently)<br>
-        Various roles, including design work, 3D modeling of houses, brochure design, web development, and field construction</p>
-     
+     <article id="pennant-creatives">
+         
+         <h3>Pennant Creatives</h3>
+         <p>
+            <strong>Founder, Web Developer & Designer</strong><br>
+            <em>July 2013 – July 2024</em>
+         </p>
+         
+         
+         <p><strong>Strategic Planning:</strong> Weekly review of agency's long-term goals and objectives. Strategized how daily activities contribute to these goals.</p>
+         <p><strong>Client Communication:</strong> Communicated with clients to discuss project updates, gather feedback, and address any concerns or questions they may have.</p>
+         <p><strong>Team Management:</strong> Checked in with the creative team to discuss project progress, assign tasks, and provide guidance and feedback.</p>
+         <p><strong>Project Management:</strong> Monitored ongoing projects to ensure they were on track, within budget, and meeting client expectations.</p>
+         <p><strong>Client Pitches & RFPs:</strong> Collaborated on presentations for client pitches and Requests for Proposals (RFPs), contributing technical insights and helping to showcase the company’s capabilities and value proposition.</p>
+         <p><strong>Creative Direction:</strong> Provided creative direction and input on projects, helping to generate innovative ideas and solutions.</p>
+         <p><strong>Financial Management:</strong> Reviewed financial statements, budgeting, and invoicing to ensure the agency's financial solvency.</p>
+         <p><strong>Quality Control:</strong> Ensured that the quality of work produced was consistent and met or exceeded industry standards.</p>
+         <p><strong>Web Development:</strong> Built all client sites on WordPress using the GeneratePress theme and GenerateBlocks theme extension.</p>
+     </article>
+     <article id="planned-giving">
+         
+         <h3>PlannedGiving.com</h3>
+         <p>
+            <strong>Web Services</strong><br>
+            July 2014 – October 2024
+         </p>
+         
+         <p><strong>Design and Template Creation:</strong> Replicated the design and user interface of the organization's website, including layout, color schemes and fonts, to create a customizable template for the dynamic integration of Planned Giving marketing content.</p>
+         <p><strong>Content Management:</strong> Regularly updated and modified content based on customer requests received via email or phone, ensuring timely and accurate adjustments.</p>
+         <p><strong>Brand Consistency:</strong> Ensured seamless user experience throughout all updates and improvements.</p>
+         <p><strong>Feature Development:</strong> Added new features to the proprietary platform to improve both the developer experience and the product offering, enhancing usability, efficiency, and overall functionality. Rewrote legacy styles and scripts when newer Web APIs became available. Platform was built in PHP and MySQL.</p>
+         
+     </article>
+
+     <article id="additional">
+         <h2>Additional Experience:</h2>
+         <p><strong>Kimberton Whole Foods</strong>, February 2009 - June 2013 (intermittently)<br>
+            Assistant Store Coordinator - Downingtown location,<br>
+            Perishables Supervisor - Warehouse location</p>
+         <p><strong>Lancaster Farm Fresh Cooperative</strong>, June 2011 - June 2012<br>
+            Operational Coordinator for Community Supported Agriculture (CSA) Program</p>
+         <p><strong>Home Field Advantage, Ltd.</strong>, January 1998 - August 2006 (intermittently)<br>
+            Various roles, including design work, 3D modeling of houses, brochure design, web development, and field construction</p>
+         
+     </article>
  </section>
-<section>
+<section id="skills-training" >
+    <h2>Skills & Training:</h2>
     
+    <article id="education">
+        <h3>Education:</h3>
+        
+        <p>
+            Bachelors of Science in General Studies, Art Concentration,  May 2006
+            Kutztown University of Pennsylvania
+        </p>
+        
+    </article>
     
-    <h2>Education:</h2>
+    <article id="computer-skills">
+        
+        <h3>Computer Skills:</h3>
+        <p>
+            Microsoft Word, Excel, Powerpoint<br>
+            Adobe Photoshop, Lightroom, Illustrator, InDesign, Premiere, XD<br>
+            Sketch<br>
+            Figma<br>
+            cPanel
+        </p>
+        
+    </article>
+
+    <article id="programing-lang">
+        <h3>Programming Languages:</h3>
+        <p>HTML, CSS, JS, PHP, MySQL</p>
+    </article>
+    <article id="programing-frame">
+        <h3>Programming Frameworks:</h3>
+        <p>WordPress, Vue</p>
+    </article>
     
-    <p>
-        Bachelors of Science in General Studies, Art Concentration,  May 2006
-        Kutztown University of Pennsylvania
-    </p>
-    
-    
-    <h2>Computer Skills:</h2>
-    <p>
-        Microsoft Word, Excel, Powerpoint<br>
-        Adobe Photoshop, Lightroom, Illustrator, InDesign, Premiere, XD<br>
-        Sketch<br>
-        Figma<br>
-        cPanel
-    </p>
-    
-    <h2>Programming Language:</h2>
-    <p>HTML, CSS, JS, PHP, MySQL</p>
-    <h2>Programming Frameworks:</h2>
-    <p>WordPress, Vue</p>
-    
-    <h2>Volunteer Work:</h2>
-    <ul>
-        <li>Lancaster Recreation Commission U8 soccer coach for the 2023 Spring and Fall seasons and 2024 Spring season.</li>
-        <li>Created and launched lanc.care during the March 2020 lockdown of the COVID pandemic: <a href="https://penncapital-star.com/labor/were-just-trying-to-fill-the-void-ease-the-pain-across-lancaster-pa-volunteers-come-together-amid-covid-19-shutdowns/" target="_blank">Story</a></li>
-        <li>Lancaster City Community Garden team member that helped build and maintain a local garden: <a href="https://lancasteronline.com/news/refugees-hope-community-garden-helps-them-put-down-roots-here/article_7a01eaa4-b2d8-5c74-859f-a4608b212182.html" target="_blank">Story</a></li>
-    </ul>
+    <article id="volunteering">
+        
+        <h3>Volunteer Work:</h3>
+        <ul>
+            <li>Lancaster Recreation Commission U8 soccer coach for the 2023 Spring and Fall seasons and 2024 Spring season.</li>
+            <li>Created and launched lanc.care during the March 2020 lockdown of the COVID pandemic: <a href="https://penncapital-star.com/labor/were-just-trying-to-fill-the-void-ease-the-pain-across-lancaster-pa-volunteers-come-together-amid-covid-19-shutdowns/" target="_blank">Story</a></li>
+            <li>Lancaster City Community Garden team member that helped build and maintain a local garden: <a href="https://lancasteronline.com/news/refugees-hope-community-garden-helps-them-put-down-roots-here/article_7a01eaa4-b2d8-5c74-859f-a4608b212182.html" target="_blank">Story</a></li>
+        </ul>
+    </article>
 </section>
 
 <section id="work">
@@ -112,36 +134,40 @@
     
     <h2>Examples of Work</h2>
     
-    <h3>Web Design & Development:</h3>
-    <ul>
-        <li>Coming soon: delcopa.gov<br><small>(design only - site in final stages of development - to be launched on or soon after November 9, 2024)</small></li>
-        <li><a href="https://careerreadylancaster.com/" target="_blank">Career Ready Lancaster!</a></li>
-        <li><a href="https://www.lancastercountywib.com/" target="_blank">Lancaster County Workforce Development Board</a></li>
-        <li><a href="https://www.uwlanc.org" target="_blank">United Way of Lancaster County</a></li>
-        <li><a href="https://oneunitedlancaster.com/" target="_blank">One United Lancaster</a></li>
-        <li><a href="https://illuccixhcp.com/" target="_blank">Illuccix HCP</a> <small>(development only)</small></li>
-        <li><a href="https://illuccix.com/" target="_blank">Illuccix Patient</a> <small>(development only)</small></li>
-        <li><a href="https://horseinnlancaster.com/" target="_blank">Horse Inn</a></li>
-        <li><a href="https://philthygood.com/" target="_blank">Philthy Good</a></li>
-        <li><a href="https://columbiapamarkethouse.org/" target="_blank">Columbia Market House</a></li>
-        <li><a href="https://first10lancaster.com/" target="_blank">First 10 Lancaster</a></li>
-        <li><a href="https://lititzpride.org/" target="_blank">Lititz Chooses Love</a></li>
-        <li><a href="https://lancastercountycase.com/" target="_blank">Lancaster County CASE</a></li>
-        <li><a href="https://jessicasmucker.com/" target="_blank">Jessica Smucker Music</a></li>
-        <li><a href="https://somervillemanning.com/" target="_blank">Somerville Manning Gallery</a></li>
-        <li><a href="https://heyjinx.com/" target="_blank">HeyJinx! Design</a></li>
-        <li><a href="https://lanc.care/" target="_blank">Lanc.Care</a></li>
-    </ul>
-    
-    <h3>Logo and Branding:</h3>
-    <ul>
-        <li><a href="https://heyjinx.com/" target="_blank">HeyJinx! Design</a></li>
-        <li><a href="https://jessicasmucker.com/" target="_blank">Jessica Smucker Music</a></li>
-        <li><a href="https://philthygood.com/" target="_blank">Philthy Good</a></li>
-        <li><a href="https://careerreadylancaster.com/" target="_blank">Career Ready Lancaster!</a></li>
-        <li><a href="https://www.bowerydevelopmentco.com/" target="_blank">Bowery Development Co.</a></li>
-        <li><a href="https://thealistair.rentpmi.com/" target="_blank">The Alistair</a> <small>(including naming of building)</small></li>
-    </ul>
+    <article id="web-dev">
+        
+        <h3>Web Design & Development:</h3>
+        <ul>
+            <li>Coming soon: delcopa.gov<br><small>(design only - site in final stages of development - to be launched on or soon after November 9, 2024)</small></li>
+            <li><a href="https://careerreadylancaster.com/" target="_blank">Career Ready Lancaster!</a></li>
+            <li><a href="https://www.lancastercountywib.com/" target="_blank">Lancaster County Workforce Development Board</a></li>
+            <li><a href="https://www.uwlanc.org" target="_blank">United Way of Lancaster County</a></li>
+            <li><a href="https://oneunitedlancaster.com/" target="_blank">One United Lancaster</a></li>
+            <li><a href="https://illuccixhcp.com/" target="_blank">Illuccix HCP</a> <small>(development only)</small></li>
+            <li><a href="https://illuccix.com/" target="_blank">Illuccix Patient</a> <small>(development only)</small></li>
+            <li><a href="https://horseinnlancaster.com/" target="_blank">Horse Inn</a></li>
+            <li><a href="https://philthygood.com/" target="_blank">Philthy Good</a></li>
+            <li><a href="https://columbiapamarkethouse.org/" target="_blank">Columbia Market House</a></li>
+            <li><a href="https://first10lancaster.com/" target="_blank">First 10 Lancaster</a></li>
+            <li><a href="https://lititzpride.org/" target="_blank">Lititz Chooses Love</a></li>
+            <li><a href="https://lancastercountycase.com/" target="_blank">Lancaster County CASE</a></li>
+            <li><a href="https://jessicasmucker.com/" target="_blank">Jessica Smucker Music</a></li>
+            <li><a href="https://somervillemanning.com/" target="_blank">Somerville Manning Gallery</a></li>
+            <li><a href="https://heyjinx.com/" target="_blank">HeyJinx! Design</a></li>
+            <li><a href="https://lanc.care/" target="_blank">Lanc.Care</a></li>
+        </ul>
+    </article>
+    <article id="branding">
+        <h3>Logo and Branding:</h3>
+        <ul>
+            <li><a href="https://heyjinx.com/" target="_blank">HeyJinx! Design</a></li>
+            <li><a href="https://jessicasmucker.com/" target="_blank">Jessica Smucker Music</a></li>
+            <li><a href="https://philthygood.com/" target="_blank">Philthy Good</a></li>
+            <li><a href="https://careerreadylancaster.com/" target="_blank">Career Ready Lancaster!</a></li>
+            <li><a href="https://www.bowerydevelopmentco.com/" target="_blank">Bowery Development Co.</a></li>
+            <li><a href="https://thealistair.rentpmi.com/" target="_blank">The Alistair</a> <small>(including naming of building)</small></li>
+        </ul>
+    </article>
 </section>
 
 <section>
@@ -193,20 +219,63 @@
 	margin-bottom: -14px;
 }
 
-  section {
-    border-top: solid 1px;
-  }
+section strong, section h2 {
+	font-weight: 900;
+}
 
   main .note {
-    margin-bottom: 30px;
+    margin-bottom: 0px;
     display: block;
   }
 
   main .note a {
     display: inline-block;
+    text-decoration-style: wavy;
+  text-decoration-thickness: 0.1rem;
+  margin-bottom: 0;
+  }
+
+  main .note + p{
+    margin-top: 0px;
   }
 
   main #work a {
 	text-decoration-thickness: 0.15rem;
+}
+
+
+article{
+  padding: 1px 0 1px 28px;
+  border-left: solid #ddd;
+  margin-left: 15px;
+  margin-bottom: 43px;
+}
+
+
+@media screen and (max-width: 668px) {
+main .note a {
+	padding: 12px 31px;
+	font-size: 1rem;
+}
+
+
+
+.pre-title {
+  font-size: 1rem;
+  margin-bottom: 0;
+}
+
+section ul {
+	padding-left: 28px;
+}
+
+section article ul {
+	padding-left: 15px;
+}
+
+section article ul li {
+	margin-top: 5px;
+}
+
 }
   </style>
